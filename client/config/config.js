6d2d2b3677abfbfb85d1fa2177b6ea4f38b90ff6
Mern-stack-developer <sonushrivastav1811@gmail.com>
2024-02-02 21:35:32 +0530

    sdsd

--- client/config/config.js
+++ client/config/config.js
@@ -1,1 +1,2 @@
-export  const LOCALHOST_URL = "http://localhost:8080/api"
+// export  const LOCALHOST_URL = "http://localhost:8080/api"
+export  const LOCALHOST_URL = "https://expense-tracker-server-bay.vercel.app/api"
